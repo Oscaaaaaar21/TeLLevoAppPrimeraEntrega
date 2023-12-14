5dd983004c6ba4856ca953dc66823021622e840e
Oscaaaaaar21 <129178186+Oscaaaaaar21@users.noreply.github.com>
2023-12-14 13:28:56 -0300

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,22 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.app',
   appName: 'TeLlevoApp',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
   }
 };
 
